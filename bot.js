4e2122498108505f1e32a08645e173fadb61d9d4
huddsfu7sfhuh <43814931+huddsfu7sfhuh@users.noreply.github.com>
2018-11-13 20:29:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`👊 `,"http://twitch.tv/X_Xz")
+client.user.setGame(`Foxmc Support`,"http://twitch.tv/X_Xz")
 });
 
 

6f4b81bc3d068fe1343423efb89e251b50337c7c
huddsfu7sfhuh <43814931+huddsfu7sfhuh@users.noreply.github.com>
2018-11-13 20:26:24 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,26 +1,9 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-
-client.on('message', message => {
-    var argresult = message.content.split(` `).slice(1).join(' ');
-      if (!devs.includes(message.author.id)) return;
-      
-  if (message.content.startsWith('$' + 'ply')) {
-    client.user.setGame(argresult);
-      message.channel.sendMessage(`**✅   ${argresult}**`)
-  } else 
-  if (message.content.startsWith('$' + 'wt')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.sendMessage(`**✅   ${argresult}**`)
-  } else 
-  if (message.content.startsWith('$' + 'ls')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.sendMessage(`**✅   ${argresult}**`)
-  } else 
-  if (message.content.startsWith('$' + 'st')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/X_Xz");
-      message.channel.sendMessage(`**✅   ${argresult}**`)
-  }
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`👊 `,"http://twitch.tv/X_Xz")
+});
 
 
 client.login(process.env.BOT_TOKEN); 

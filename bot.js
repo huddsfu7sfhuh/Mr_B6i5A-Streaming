cb0091a4dcd35443ed5e61aaa7960dcab83307b4
huddsfu7sfhuh <43814931+huddsfu7sfhuh@users.noreply.github.com>
2018-11-15 14:28:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,33 +1,35 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
-const prefix = '.'
- 
+
+const Util = require('discord.js');
+    client = new Discord.Client({sisableEveryone: true})
+ const devs = ['441623408707371020'];
+const adminprefix = "*";
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!devs.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'play')) {
+    client.user.setGame(argresult);
+      message.channel.sendMessage(`**  ${argresult} تم تغيير الحاله الي ✅**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.sendMessage(`**  ${argresult} تم تغيير الحاله الي ✅**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.sendMessage(`**  ${argresult} تم تغيير الحاله الي ✅**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/Taaino");
+      message.channel.sendMessage(`**  ${argresult} تم تغيير الحاله الي ✅**`)
+  }
+  });
+
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Foxmc Support`,"http://twitch.tv/S-F")
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
+
+console.log('D7i Stream Start');
+
 });
 
-
-
-client.login(process.env.BOT_TOKEN); 
+client.login(process.env.BOT_TOKEN);
